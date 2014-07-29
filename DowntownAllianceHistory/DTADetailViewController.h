c67303457db99aad9e573b4cf3f18c45d643812e
Viktor Falkner <vfalkner@live.unc.edu>
2014-07-29 14:05:42 -0400

    Completed DTADetailViewController

---
+++ DowntownAllianceHistory/DTADetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DTADetailViewController.h
+//  DowntownAllianceHistory
+//
+//  Created by Viktor Falkner on 7/29/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DTADetailViewController : UIViewController
+
+@end
