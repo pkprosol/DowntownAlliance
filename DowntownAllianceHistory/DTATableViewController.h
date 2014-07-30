6f91e52254eeaa602e8da725480ade170ccf28d9
Rmilian <r-milian@hotmail.com>
2014-07-30 10:01:15 -0400

    tvc

---
+++ DowntownAllianceHistory/DTATableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DTATableViewController.h
+//  DowntownAllianceHistory
+//
+//  Created by flatiron on 7/30/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DTATableViewController : UITableViewController
+
+@end
