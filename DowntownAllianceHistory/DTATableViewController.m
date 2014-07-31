a600aaab02f629362081c2418039e7634516de17
Rmilian <r-milian@hotmail.com>
2014-07-31 11:23:35 -0400

    hidableTab

--- DowntownAllianceHistory/DTATableViewController.m
+++ DowntownAllianceHistory/DTATableViewController.m
@@ -23,6 +23,37 @@
     return self;
 }
 
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+        self.hidesBottomBarWhenPushed = YES;
+        self.navigationController.navigationBarHidden = NO;
+    }
+    return self;
+}
+
+
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self.navigationController setNavigationBarHidden:NO animated:YES];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
