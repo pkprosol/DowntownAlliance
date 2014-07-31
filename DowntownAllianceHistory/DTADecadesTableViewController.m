28cca1307092639d1302d5757f94ac79075a9fc5
Viktor Falkner <vfalkner@live.unc.edu>
2014-07-31 13:58:53 -0400

    Completed PS'd Cells and added to Array to populate Decade tableview

--- DowntownAllianceHistory/DTADecadesTableViewController.m
+++ DowntownAllianceHistory/DTADecadesTableViewController.m
@@ -36,16 +36,62 @@
     return self;
 }
 
-//- (void)viewDidLoad
-//{
-//    [super viewDidLoad];
-//
-//    // Uncomment the following line to preserve selection between presentations.
-//    // self.clearsSelectionOnViewWillAppear = NO;
-//
-//    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-//    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-//}
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+//    NSMutableArray *arrayOfDecadesPhotos = [NSMutableArray arrayWithObjects:[UIImage imageNamed:@"1900s.jpg"],[UIImage imageNamed:@"1910s.jpg"],[UIImage imageNamed:@"1920s.jpg"],[UIImage imageNamed:@"1930s.jpg"], [UIImage imageNamed:@"1940s.jpg"], [UIImage imageNamed:@"1950s.jpg"], [UIImage imageNamed:@"1960s.gif"], [UIImage imageNamed:@"1970s.jpg"], [UIImage imageNamed:@"1980s"], [UIImage imageNamed:@"1990s.jpg"], [UIImage imageNamed:@"2000s.jpg"], nil];
+    
+    NSMutableArray *arrayOfDecadesPhotos = [NSMutableArray arrayWithObjects:[UIImage imageNamed:@"1910s"],[UIImage imageNamed:@"1920s"],[UIImage imageNamed:@"1930s"],[UIImage imageNamed:@"1940s"], [UIImage imageNamed:@"1950's"], [UIImage imageNamed:@"1960s"], [UIImage imageNamed:@"1970s"], [UIImage imageNamed:@"1980s"], [UIImage imageNamed:@"1990s"], [UIImage imageNamed:@"2000s"],[UIImage imageNamed:@"2010s"], nil] ;
+
+    self.decadesPhotoArray = arrayOfDecadesPhotos;
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    return [self.decadesPhotoArray count];
+}
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 100.0f;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    DTAFilterCell *cell = [tableView dequeueReusableCellWithIdentifier:@"basicCell" forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    UIImage *imageOfDecade = self.decadesPhotoArray[indexPath.row];
+    
+    cell.filterCellImageView.contentMode = UIViewContentModeScaleAspectFill;
+    
+    cell.filterCellImageView.image = imageOfDecade;
+    
+    return cell;
+}
 
 - (void)viewDidUnload
 {
@@ -82,7 +128,7 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     [tableView  deselectRowAtIndexPath:indexPath animated:YES];
-
+    
 }
 
 #pragma mark - The Magic!
@@ -130,7 +176,7 @@
     CGFloat differenceFromStart = startContentOffset - currentOffset;
     CGFloat differenceFromLast = lastContentOffset - currentOffset;
     lastContentOffset = currentOffset;
-        
+    
     if((differenceFromStart) < 0)
     {
         // scroll up
@@ -156,61 +202,6 @@
 {
     [self contract];
     return YES;
-}
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-
-    NSMutableArray *arrayOfDecadesPhotos = [NSMutableArray arrayWithObjects:[UIImage imageNamed:@"1900s.jpg"],[UIImage imageNamed:@"1910s.jpg"],[UIImage imageNamed:@"1920s.jpg"],[UIImage imageNamed:@"1930s.jpg"], [UIImage imageNamed:@"1940s.jpg"], [UIImage imageNamed:@"1950s.jpg"], [UIImage imageNamed:@"1960s.gif"], [UIImage imageNamed:@"1970s.jpg"], [UIImage imageNamed:@"1980s"], [UIImage imageNamed:@"1990s.jpg"], [UIImage imageNamed:@"2000s.jpg"], nil];
-
-    self.decadesPhotoArray = arrayOfDecadesPhotos;
-
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-#pragma mark - Table view data source
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-    // Return the number of sections.
-    return 1;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    // Return the number of rows in the section.
-    return [self.decadesPhotoArray count];
-}
--(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    return 250.0f;
-}
-
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    DTAFilterCell *cell = [tableView dequeueReusableCellWithIdentifier:@"basicCell" forIndexPath:indexPath];
-    
-    // Configure the cell...
-    
-    UIImage *imageOfDecade = self.decadesPhotoArray[indexPath.row];
-    
-    cell.filterCellImageView.contentMode = UIViewContentModeScaleAspectFill;
-    
-    cell.filterCellImageView.image = imageOfDecade;
-    
-    return cell;
 }
 
 /*
