fb82a5ed831861575b370d90dbae8b1948a40a0d
Viktor Falkner <vfalkner@live.unc.edu>
2014-08-01 15:04:27 -0400

    added index 2 of detail cell

---
+++ DTAImageTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  DTAImageTableViewCell.h
+//  DowntownAllianceHistory
+//
+//  Created by Viktor Falkner on 8/1/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DTAImageTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *pictureImage;
+
+@end
---
+++ DowntownAllianceHistory/DTAImageViewCell.h
@@ -0,0 +1,13 @@
+//
+//  DTAImageViewCell.h
+//  DowntownAllianceHistory
+//
+//  Created by Viktor Falkner on 8/1/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DTAImageViewCell : UITableViewCell
+
+@end
