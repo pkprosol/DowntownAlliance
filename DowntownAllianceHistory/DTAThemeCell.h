4e36d8374684f98b70e1d4dc8cd860aed14bb533
Viktor Falkner <vfalkner@live.unc.edu>
2014-08-01 17:35:02 -0400

    created a new Cell Class - crashes on tab switch

---
+++ DowntownAllianceHistory/DTAThemeCell.h
@@ -0,0 +1,14 @@
+//
+//  DTAThemeCell.h
+//  DowntownAllianceHistory
+//
+//  Created by Viktor Falkner on 8/1/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DTAThemeCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *themeImageView;
+
+@end
--- DowntownAllianceHistory/DTAThemesTableViewController.h
+++ DowntownAllianceHistory/DTAThemesTableViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "DTAThemeCell.h"
 
 @interface DTAThemesTableViewController : UITableViewController
 
