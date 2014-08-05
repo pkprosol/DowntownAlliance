61898876fb9ddb79503baffb2d8612366a52a8bb
Rmilian <r-milian@hotmail.com>
2014-08-05 14:42:02 -0400

    geofencing done

---
+++ DowntownAllianceHistory/DTAGeoFencing.h
@@ -0,0 +1,13 @@
+//
+//  DTAGeoFencing.h
+//  DowntownAllianceHistory
+//
+//  Created by flatiron on 8/5/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DTAGeoFencing : NSObject
+
+@end
---
+++ DowntownAllianceHistory/DTAGeoFencing.m
@@ -0,0 +1,13 @@
+//
+//  DTAGeoFencing.m
+//  DowntownAllianceHistory
+//
+//  Created by flatiron on 8/5/14.
+//
+//
+
+#import "DTAGeoFencing.h"
+
+@implementation DTAGeoFencing
+
+@end
