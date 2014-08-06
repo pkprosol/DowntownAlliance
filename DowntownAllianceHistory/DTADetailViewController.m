f77d4070d986e588df8ad8027828da4f36917868
Viktor Falkner <vfalkner@live.unc.edu>
2014-08-06 14:14:30 -0400

    shit to fix

--- DowntownAllianceHistory/DTADetailViewController.m
+++ DowntownAllianceHistory/DTADetailViewController.m
@@ -55,6 +55,7 @@
     _mapOutlet.delegate = self;
     _tableViewOutlet.delegate = self;
     _tableViewOutlet.dataSource = self;
+    self.tableViewOutlet.backgroundColor = [UIColor clearColor];
     // Do any additional setup after loading the view.
 }
 
@@ -83,19 +84,22 @@
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"detailCell" forIndexPath:indexPath];
     
-    if (indexPath.row == 0) {
-        cell.textLabel.text = self.locationToBePLotted.titleOfPlaque;
-    }
-    else if (indexPath.row == 1) {
-        cell.textLabel.text = self.locationToBePLotted.brochureDescription;
-    }
-    else if (indexPath.row ==2) {
-        
-        DTAImageTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"pictureImage"forIndexPath:indexPath];
-        
-        cell.pictureImage.contentMode = UIViewContentModeScaleAspectFill;
-        cell.pictureImage.image = self.locationToBePLotted.image;
-    }
+//    if (indexPath.row == 0) {
+//        cell.textLabel.text = self.locationToBePLotted.titleOfPlaque;
+//    }
+//    else if (indexPath.row == 1) {
+//        cell.textLabel.text = self.locationToBePLotted.brochureDescription;
+//    }
+//    else if (indexPath.row ==2) {
+//        
+//        DTAImageTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"pictureImage"forIndexPath:indexPath];
+//        
+//        cell.pictureImage.contentMode = UIViewContentModeScaleAspectFill;
+//        cell.pictureImage.image = self.locationToBePLotted.image;
+//    }
+    NSString *shit = [NSString stringWithFormat:@"%d", indexPath.row];
+    
+    cell.textLabel.text = shit;
     
     return cell;
 }
@@ -111,6 +115,19 @@
     pointToAnnotate.title = locationToBePlotted.idNumber;
     
     [self.mapOutlet addAnnotation:pointToAnnotate];
+}
+#pragma mark - Overlap
+
+-(void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    self.tableViewOutlet.contentInset = UIEdgeInsetsMake(self.tableViewOutlet.frame.size.height-40, 0, 0, 0);
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    if (scrollView.contentOffset.y < self.mapOutlet.frame.size.height*-1 ) {
+        [scrollView setContentOffset:CGPointMake(scrollView.contentOffset.x, self.mapOutlet.frame.size.height*-1)];
+        
+    }
 }
 /*
 #pragma mark - Navigation
---
+++ DowntownAllianceHistory/DTAScrollingDetailTable.m
@@ -0,0 +1,39 @@
+//
+//  DTAScrollingDetailTable.m
+//  DowntownAllianceHistory
+//
+//  Created by Viktor Falkner on 8/6/14.
+//
+//
+
+#import "DTAScrollingDetailTable.h"
+
+@implementation DTAScrollingDetailTable
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
+    id hitView = [super hitTest:point withEvent:event];
+    if (point.y<0) {
+        return nil;
+    }
+    return hitView;
+}
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
