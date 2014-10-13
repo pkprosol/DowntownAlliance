7029141dcfa9ba9290bf0f62436597c78b47ac99
Piotr Prosol <Home@Piotrs-MacBook-Pro.local>
2014-10-13 17:43:57 -0400

    Cleaned up Scrolling Detail TVC

--- DowntownAllianceHistory/DTAScrollingDetailTable.m
+++ DowntownAllianceHistory/DTAScrollingDetailTable.m
@@ -10,30 +10,12 @@
 
 @implementation DTAScrollingDetailTable
 
-- (id)initWithFrame:(CGRect)frame
-{
-    self = [super initWithFrame:frame];
-    if (self) {
-        // Initialization code
-    }
-    return self;
-}
-
-
 - (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
     id hitView = [super hitTest:point withEvent:event];
-    if (point.y<0) {
+    if (point.y < 0) {
         return nil;
     }
     return hitView;
 }
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
-}
-*/
 
 @end
