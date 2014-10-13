02724fdf159a8d6418a14fbf021ffa1696e86f1d
Piotr Prosol <Home@Piotrs-MacBook-Pro.local>
2014-10-13 17:46:30 -0400

    Cleaned up Cells and Xibs (partially)

--- DowntownAllianceHistory/DTAListCell.m
+++ DowntownAllianceHistory/DTAListCell.m
@@ -10,27 +10,6 @@
 
 @implementation DTAListCell
 
-- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
-{
-    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
-    if (self) {
-        // Initialization code
-    }
-    return self;
-}
-
-- (void)awakeFromNib
-{
-    // Initialization code
-}
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated
-{
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
-}
-
 - (void)layoutSubviews
 {
     [super layoutSubviews];
